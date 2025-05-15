2b2ceeed2baf45515c86cca7f9a5df6b683f4d50
Akram BARRA <kovee7@proton.me>
2025-05-15 10:47:39 +0200

    Added PlayerActions enum containing each possible action for the player

---
+++ src/main/java/fr/iut/hev/root/model/enums/PlayerActions.java
@@ -0,0 +1,22 @@
+package fr.iut.hev.root.model.enums;
+
+public enum PlayerActions {
+    USE_ITEM,
+    INTERACT,
+    MOVE_RIGHT,
+    MOVE_LEFT,
+    MOVE_UP,
+    MOVE_DOWN,
+    JUMP,
+    INVENTORY,
+    HOTBAR_1,
+    HOTBAR_2,
+    HOTBAR_3,
+    HOTBAR_4,
+    HOTBAR_5,
+    HOTBAR_6,
+    HOTBAR_7,
+    HOTBAR_8,
+    HOTBAR_9,
+    HOTBAR_10;
+}
