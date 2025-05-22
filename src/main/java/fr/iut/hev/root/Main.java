dd3ec90b2b31885b25a5e5ae13e57bbf1e1ecf23
Zar0102 <lino.couturier@laposte.net>
2025-05-22 09:49:48 +0200

    Adding main

---
+++ src/main/java/fr/iut/hev/root/Main.java
@@ -0,0 +1,26 @@
+package fr.iut.hev.root;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public static Scene scene;
+//--enable-native-access=javafx.graphics
+    @Override
+    public void start(Stage stage) throws IOException {
+        Pane root = FXMLLoader.load(Main.class.getResource("view/globalView.fxml"));
+        scene = new Scene(root, 1920, 1056);
+        stage.setTitle("ROOT");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
